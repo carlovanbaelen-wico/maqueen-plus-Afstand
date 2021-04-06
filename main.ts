8fba1ff509f182d397198780ef3ee92a0a637fb1
carlovanbaelen-wico <67262093+carlovanbaelen-wico@users.noreply.github.com>
2021-04-06 14:55:02 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,20 @@
+let direction = false
+let U = 0
+DFRobotMaqueenPluss.PID(PID.OFF)
+basic.forever(function () {
+    U = DFRobotMaqueenPluss.ultraSonic(PIN.P8, PIN.P12)
+    if (U < 20 && U != 0) {
+        direction = Math.randomBoolean()
+        if (direction == true) {
+            DFRobotMaqueenPluss.mototRun(Motors.M1, Dir.CW, 50)
+            DFRobotMaqueenPluss.mototRun(Motors.M2, Dir.CW, 0)
+            basic.pause(1000)
+        } else {
+            DFRobotMaqueenPluss.mototRun(Motors.M1, Dir.CW, 0)
+            DFRobotMaqueenPluss.mototRun(Motors.M2, Dir.CW, 50)
+            basic.pause(1000)
+        }
+    } else {
+        DFRobotMaqueenPluss.mototRun(Motors.ALL, Dir.CW, 50)
+    }
+})
